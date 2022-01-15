5722e6e6eb499d76543a03814be34ec313a84e03
calebtt <mounteriemachine@hotmail.com>
2022-01-15 09:36:29 -0600

    Added FillContainerRandom template.

--- BuildRandom.hpp
+++ BuildRandom.hpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <limits>
 #include <numeric>
+#include <ranges>
 
 namespace BuildRandom
 {
@@ -14,14 +15,12 @@ namespace BuildRandom
 	using StringType = std::string;
 	using WStringType = std::wstring;
 
-	/// <summary>
-	/// Returns a vector of type <c>T</c> with randomized content using a uniform distribution.
-	///	T must be default constructable.
-	/// </summary>
+	/// <summary>Returns a vector of a random number of type <c>T</c> with randomized content using a uniform distribution. T must be default constructable.</summary>
 	///	<param name="maxLength">the maximum count of the type T in the returned vector.</param>
 	///	<param name="minLength">the minimum count of the type T in the returned vector.</param>
 	/// <returns> a vector of type T with randomized content. Empty vector on error. </returns>
 	template<typename T>
+	requires std::is_integral_v<T> && std::is_pod_v<T> && std::is_trivial_v<T>
 	[[nodiscard]] constexpr static auto BuildRandomVector(const CountType maxLength, const CountType minLength = 3) noexcept
 	{
 		//arg error checking, returns empty vector as per description
@@ -45,20 +44,48 @@ namespace BuildRandom
 		};
 		//the distribution uses the generator engine to get the value
 		const auto tLength = static_cast<std::size_t>(distLengthPossibility(stringGenerator));
-		std::vector<T> currentBuiltString(tLength, T{});
-		std::generate(std::begin(currentBuiltString), std::end(currentBuiltString), getRandomElement);
-		return currentBuiltString;
+		std::vector<T> currentBuiltVector(tLength, T{});
+		std::generate(std::begin(currentBuiltVector), std::end(currentBuiltVector), getRandomElement);
+		return currentBuiltVector;
 	}
-	/// <summary>
-	///	Returns a vector of <c>std::string</c> with randomized content using a uniform distribution.
-	///	<para>NOTE:
+	/// <summary>Fills a container of type <c>T</c> with randomized content using a uniform distribution. T must be default constructable.</summary>
+	///	<param name="maxLength">the maximum count of the type T in the returned vector.</param>
+	///	<param name="minLength">the minimum count of the type T in the returned vector.</param>
+	/// <returns> true on success, false on error.</returns>
+	template<typename T>
+	[[nodiscard]] constexpr static bool FillContainerRandom(std::ranges::range auto &containerType, const CountType maxLength, const CountType minLength = 3) noexcept
+	{
+		//arg error checking, returns false as per description
+		if (minLength >= maxLength || (maxLength <= 0) || (minLength <= 0))
+		{
+			return false;
+		}
+		//Test all kinds of random type T possibilities.
+		std::uniform_int_distribution<> distElementPossibility
+		(std::numeric_limits<T>::min(),
+			std::numeric_limits<T>::max());
+
+		const std::uniform_int_distribution<> distLengthPossibility(minLength, maxLength);
+		std::random_device rd;
+		std::mt19937 randomElementGenerator(rd()); // seed mersenne engine
+
+		//lambda used with std::generate to fill the vector with type T of randomized content.
+		const auto getRandomElement = [&distElementPossibility, &randomElementGenerator]()
+		{
+			return static_cast<T>(distElementPossibility(randomElementGenerator));
+		};
+		//the distribution uses the generator engine to get the value
+		const auto tLength = static_cast<std::size_t>(distLengthPossibility(randomElementGenerator));
+		containerType.resize(tLength);
+		std::generate(std::begin(containerType), std::end(containerType), getRandomElement);
+		return true;
+	}
+	/// <summary>Returns a vector of <c>std::string</c> with randomized content using a uniform distribution.<para>NOTE:
 	///	The char type is a character representation type that efficiently encodes members of the basic execution character set.
 	///	The C++ compiler treats variables of type char, signed char, and unsigned char as having different types.
 	///	<para>***Microsoft - specific: Variables of type char are promoted to int as if from type signed char by default,
 	///	unless the / J compilation option is used. In this case, they're treated as type unsigned char and are promoted to int without sign extension.***</para>
-	///	</para>
-	///	<para><a href="https://docs.microsoft.com/en-us/cpp/cpp/fundamental-types-cpp/">MSDOCS</a></para>
-	/// </summary>
+	///	</para><para><a href="https://docs.microsoft.com/en-us/cpp/cpp/fundamental-types-cpp/">MSDOCS</a></para></summary>
 	///	<param name="numberOfStrings">the number of strings in the returned vector.</param>
 	///	<param name="maxLength">the maximum length of the strings in the returned vector.</param>
 	///	<param name="minLength">the minimum length of the strings in the returned vector.</param>
@@ -78,9 +105,7 @@ namespace BuildRandom
 		}
 		return ret;
 	}
-	/// <summary>
-	///	Returns a vector of <c>std::wstring</c> with randomized content using a uniform distribution.
-	/// </summary>
+	/// <summary>Returns a vector of <c>std::wstring</c> with randomized content using a uniform distribution.</summary>
 	///	<param name="numberOfStrings">the number of strings in the returned vector.</param>
 	///	<param name="maxLength">the maximum length of the strings in the returned vector.</param>
 	///	<param name="minLength">the minimum length of the strings in the returned vector.</param>
--- BuildRandomLib.cpp
+++ BuildRandomLib.cpp
@@ -2,12 +2,15 @@
 //
 
 #include <iostream>
+#include <list>
+
 #include "BuildRandom.hpp"
 
 
 int main()
 {
 	using namespace std;
+	//building a vector of strings with 10 strings of length between 3 and 5 characters.
 	const auto stringVec = BuildRandom::BuildRandomStringVector(10, 5);
 	cout << "Printing some character values, the default char type for std::string might be signed..." << endl;
 	for_each(begin(stringVec), end(stringVec), [](const auto &selem)
@@ -19,13 +22,15 @@ int main()
 			cout << endl;
 		});
 	cout << endl << endl;
-	const auto stringVec2 = BuildRandom::BuildRandomVector<unsigned char>(10, 5);
+	//building a vector of random size (between the arg values) with random data.
+	const auto stringVec2 = BuildRandom::BuildRandomVector<unsigned char>(100, 5);
 	cout << "Printing some unsigned character values." << endl;
 	for_each(begin(stringVec2), end(stringVec2), [](const auto elem)
 		{
 			cout << std::dec << "[" << static_cast<const int>(elem) << "]";
 		});
 	cout << endl << endl;
+	//building a vector of wstrings (wide char) with 10 strings of length between 3 and 5 characters.
 	const auto stringVec3 = BuildRandom::BuildRandomWStringVector(10, 5);
 	cout << "Printing some wide char values." << endl;
 	for_each(begin(stringVec3), end(stringVec3), [](const auto selem)
@@ -36,4 +41,23 @@ int main()
 				});
 			cout << endl;
 		});
+	cout << endl << endl;
+	//filling a vector<char> with random characters between 5 and 100 elements long.
+	std::vector<char> fillVector;
+	const bool result = BuildRandom::FillContainerRandom<char>(fillVector, 100, 5);
+	cout << "Printing some character values from a std::vector." << endl;
+	for_each(begin(fillVector), end(fillVector), [](const auto elem)
+		{
+			cout << std::dec << "[" << static_cast<const int>(elem) << "]";
+		});
+	cout << endl << endl;
+	//filling a list<char> with random characters between 5 and 100 elements long.
+	list<char> fillList;
+	const bool result2 = BuildRandom::FillContainerRandom<char>(fillList, 100, 5);
+	cout << "Printing some character values from a std::list." << endl;
+	for_each(begin(fillList), end(fillList), [](const auto elem)
+		{
+			cout << std::dec << "[" << static_cast<const int>(elem) << "]";
+		});
+	cout << endl << endl;
 }
